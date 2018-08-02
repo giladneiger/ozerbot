a7693398d3ad4d92c5f1829d18ef23fb4162e5a4
omrikap <omrikap@gmail.com>
2018-08-03 01:06:12 +0300

    added FeatureNeiger WIP

---
+++ src/main/java/com/psu/FeatureNeiger.java
@@ -0,0 +1,11 @@
+package com.psu;
+
+/**
+ * Created by Omri on 8/3/2018
+ */
+public class FeatureNeiger implements FeatureHandler {
+    @Override
+    public String handle() {
+        return "";
+    }
+}

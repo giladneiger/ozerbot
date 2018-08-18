26c8ebc929755df4236d3790214262ce45492e08
omrikap <omrikap@gmail.com>
2018-08-18 22:31:55 +0300

    added the ozerbot feature handler

---
+++ src/main/java/com/psu/OzerBot.java
@@ -0,0 +1,22 @@
+package com.psu;
+
+import java.util.HashMap;
+
+/**
+ * Created by Omri on 8/18/2018
+ */
+public class OzerBot {
+
+    private HashMap<String, FeatureHandler> features = new HashMap<>();
+
+    public OzerBot(HashMap<String, FeatureHandler> features) {
+        this.features = features;
+//        features.put("drunk", new DrunkFeature());  // example
+    }
+
+    public String sayToOzer(String featureName) {
+        FeatureHandler feature = features.get(featureName);
+        return feature.handle();
+    }
+
+}

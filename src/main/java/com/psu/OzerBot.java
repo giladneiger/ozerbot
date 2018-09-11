04cb9e5d470f4ac535332111daf8135c6c8fb62f
omrikap <omrikap@gmail.com>
2018-09-11 20:37:44 +0300

    cleaned comments

--- src/main/java/com/psu/OzerBot.java
+++ src/main/java/com/psu/OzerBot.java
@@ -1,18 +1,16 @@
 package com.psu;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Created by Omri on 8/18/2018
  */
-public class OzerBot {  // todo add init method that gets all the features
+public class OzerBot {
 
     private Map<String, FeatureHandler> features;
 
     public OzerBot(Map<String, FeatureHandler> features) {
         this.features = features;
-//        features.put("drunk", new DrunkFeature());  // example
     }
 
     public String sayToOzer(String featureName) {

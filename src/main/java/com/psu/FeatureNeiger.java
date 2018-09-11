fba7c6343cd67ef9a6838d50b642fcafe21ebd6f
omrikap <omrikap@gmail.com>
2018-09-11 19:19:56 +0300

    implemented handle

--- src/main/java/com/psu/FeatureNeiger.java
+++ src/main/java/com/psu/FeatureNeiger.java
@@ -1,11 +1,19 @@
 package com.psu;
 
+import java.util.Random;
+
 /**
  * Created by Omri on 8/3/2018
  */
 public class FeatureNeiger implements FeatureHandler {
     @Override
     public String handle() {
-        return "";
+        Random random = new Random();
+        int timesToSayNeiger = random.nextInt(9);
+        StringBuilder stringBuilder = new StringBuilder("Neiger");
+        for (int i = 0; i < timesToSayNeiger; i++) {
+            stringBuilder.append(" Neiger");
+        }
+        return stringBuilder.toString();
     }
 }

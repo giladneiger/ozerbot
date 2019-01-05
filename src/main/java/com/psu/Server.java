c773f1a55a986c613cc3b86efefd00f700db489e
Gilad Neiger <giladneiger11@gmail.com>
2019-01-05 19:56:47 +0200

    added ozerbot to the server, added initbot, added router

--- src/main/java/com/psu/Server.java
+++ src/main/java/com/psu/Server.java
@@ -3,6 +3,11 @@ package com.psu;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Context;
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Omri on 10/20/2018
@@ -10,11 +15,14 @@ import io.vertx.core.Vertx;
 
 public class Server extends AbstractVerticle {
 
+    private OzerBot ozerBot;
+    private Map<String, FeatureHandler> featureHandlerMap;
+
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
         Context context = Vertx.currentContext();
-
         Server server = new Server();
+        server.initBot();
         server.init(vertx, context);
         server.start();
     }
@@ -22,10 +30,28 @@ public class Server extends AbstractVerticle {
     @Override
     public void start() {
         Router router = Router.router(vertx);
+        router.get("/:command").handler(this::botHandler);
         vertx.createHttpServer().requestHandler(req -> {
             req.response()
                     .putHeader("content-type", "text/plain")
                     .end("Hello from Vert.x!");
         }).listen(8080);
+    }
+
+    private void initBot() {
+        featureHandlerMap = initializeFeatures();
+        ozerBot = new OzerBot(featureHandlerMap);
+    }
+
+    private Map<String,FeatureHandler> initializeFeatures()
+    {
+        HashMap<String, FeatureHandler> featureHandlerMap = new HashMap<>();
+        featureHandlerMap.put("drunk", new DrunkFeature());
+        featureHandlerMap.put("neiger", new NeigerFeature());
+        return featureHandlerMap;
+    }
+
+    private void botHandler(RoutingContext routingContext) {
+
     }
 }
